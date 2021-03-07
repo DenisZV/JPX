d30ba23447c1c1b034fe0eb09db35ee088df04f4
Денис <kadzukuma@rambler.ru>
2021-03-07 14:56:49 +0900

    Add telegram-clicker

--- Python/telegram-clicker.py
+++ Python/telegram-clicker.py
@@ -5,12 +5,12 @@ if len(sys.argv) >= 2:
   api_id = sys.argv[1]
   api_hash = sys.argv[2]
 
-  client = TelegramClient('pantini', api_id, api_hash)
+  client = TelegramClient('kadzukumaBotTrip', api_id, api_hash)
 
   @client.on(events.NewMessage("https://t.me/pantini_spbex_news"))
   async def new_message_event_handler(event):
     if event.message.buttons is not None:
-      await event.message.click(0, 1)
+      await event.message.click(0, 0)
 
   client.start()
   client.run_until_disconnected()
